4a35424e93f4879debcb4ee70c2426deaec4d1f6
Benjamindemiri <benjaminoforiatta4@gmail.com>
2026-01-05 00:01:25 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -17,6 +17,27 @@ document.querySelectorAll('.nav-links a').forEach(link => {
 document.getElementById('callBtn').addEventListener('click', function(e) {
     e.preventDefault(); // Prevent default link behavior
     // Your phone number here
-    let phoneNumber = '+233501234567';
+    let phoneNumber = '+233531631955';
     window.location.href = 'tel:' + phoneNumber;
+});
+// Create the overlay element dynamically
+const overlay = document.createElement('div');
+overlay.classList.add('fullscreen-overlay');
+document.body.appendChild(overlay);
+
+// Close overlay when clicking on it
+overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    overlay.innerHTML = '';
+});
+
+// Add click event to all position images
+document.querySelectorAll('.position-item img').forEach(img => {
+    img.addEventListener('click', () => {
+        const fullImg = document.createElement('img');
+        fullImg.src = img.src;
+        overlay.innerHTML = '';
+        overlay.appendChild(fullImg);
+        overlay.style.display = 'flex';
+    });
 });
